0b4da3edc4c3f051a7238b9f5f471aa2b9224ffd
Mariig16 <89750064+Mariig16@users.noreply.github.com>
2022-09-03 22:07:46 -0500

    localStorage

--- app.js
+++ app.js
@@ -1,55 +1,72 @@
-class Paciente{
-  constructor (documento, nombre){
-    this.documento = Number(documento);
-    this.nombre = nombre;
+const keyLocalStorage = "listaPacientes";
+document.addEventListener("DOMContentLoaded", () => {
+  let pacientes = []; // El arreglo global que vamos a manejar
+	
+  // Declaración de elementos del DOM
+	const $contenedorPacientes = document.querySelector("#contenedorPacientes"),
+	$btnGuardarPaciente = document.querySelector("#btnAgregarPaciente"),
+	$newPacienteDocumento = document.querySelector("#idocumento"),
+  $newPacienteNombre = document.querySelector("#inombre");
+  // Escuchar clic del botón para agregar nuevo paciente
+  $btnGuardarPaciente.onclick = () => {
+    const pDocument = $newPacienteDocumento.value;
+    const pNombre = $newPacienteNombre.value;
+    if (!pDocument && pNombre) {
+      return;
   }
-}
-const paciente = [];
-key = paciente;
+  pacientes.push({
+    document: pDocument, 
+    nombre: pNombre,
+    terminada: false,
+  });
+  $newPacienteDocumento.value = "";
+  $newPacienteNombre.value = "";
+  guardarPacientesEnLocal();
+  refrescarListaDePacientes();
+  
+  };
+  const obtenerPacientesDeAlmacenamiento = () => {
+		const posibleLista = JSON.parse(localStorage.getItem(keyLocalStorage));
+		if (posibleLista) {
+      console.log(posibleLista);
+			return posibleLista;
+		} else {
+			return [];
+		}
+	};
 
-//Defino el DOM
-let formulario = document.getElementById("formu");
-formulario.addEventListener('submit', datosFormu);
+  const guardarPacientesEnLocal = () => {
+		localStorage.setItem(keyLocalStorage, JSON.stringify(pacientes));
+    
+	};
+  //Líneas posibles para eliminar registros (Validar)
+  const refrescarListaDePacientes = () => {
+		$contenedorPacientes.innerHTML = "";
+		for (const [indice, paciente] of pacientes.entries()) {
+			const $enlaceParaEliminar = document.createElement("a");
+			$enlaceParaEliminar.classList.add("enlace-eliminar");
+			$enlaceParaEliminar.innerHTML = "&times;";
+			$enlaceParaEliminar.href = "";
+			$enlaceParaEliminar.onclick = (evento) => {
+				evento.preventDefault();
+				if (!confirm("¿Eliminar tarea?")) {
+					return;
+				}
+				pacientes.splice(indice, 1);
+				// Guardar los cambios
+				guardarTareasEnAlmacenamiento(pacientes);
+				refrescarListaDeTareas();
+			};
+      let caja = document.createElement('tbody');
+      caja.innerHTML = `<tbody><td>${paciente.document}</td>
+                        <td>${paciente.nombre}</td></tbody>`;
+      $contenedorPacientes.appendChild(caja);
+		}
+  };
+	// Llamar a la función la primera vez
+  
+	pacientes = obtenerPacientesDeAlmacenamiento();
+	refrescarListaDePacientes();
+  console.log("Hola soy la lista", pacientes);
 
-//validar que se ingresen datos
-function validar(){
-  documentoP = formu.children[1].value;
-  nombreP = formu.children[2].value;
-  if (documentoP == '' || nombreP == ''){
-    alert("Error, debe ingresar todos los datos");
-    bandera = false;
-  }else{
-    bandera = true;
-  }
-}
-
-function datosFormu(e){
-  e.preventDefault();
-  validar();
-  if (bandera == true){
-    let formu = e.target;
-    paciente.push(new Paciente(documentoP, nombreP));
-    formu.children[1].value = '';
-    formu.children[2].value = '';
-    console.log(paciente);
-    const changeString = JSON.stringify(key); 
-    localStorage.setItem("key", changeString);
-    agregarPaciente();
-  }
-}
-
-function agregarPaciente(){
-  if (localStorage != null){
- 
-  }
-  JSON.parse(localStorage.getItem(paciente))
-  if(paciente == null){
-    console.log("No hay datos registrados, se viene un nuevo registro")
-  }else{
-    let caja = document.createElement('tbody');
-    caja.innerHTML = `<tbody><td>${documentoP}</td>
-                      <td>${nombreP}</td></tbody>`;
-    tabla.appendChild(caja);
-  }
-
-}
+});
